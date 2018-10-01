c12dbd384a375949d1223c57dda7bc0f5df714dd
Luka Klacar <lukaklacar@gmail.com>
2018-10-01 11:29:31 +0200

    Updated template

--- setup.js
+++ setup.js
@@ -1,20 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJson = require('./package.json');
-
 const deleteFile = fileName => fs.unlinkSync(path.join(__dirname, fileName));
 const writeFile = (fileName, data) => fs.writeFileSync(path.join(__dirname, fileName), data);
 
-console.log('🔄 Setting up...');
-packageJson.scripts.tsc = 'tsc';
+function addDependencies() {
+  const packageJson = require('./package.json');
+  const additionalDependencies = require('./dependencies');
+  Object.assign(packageJson.devDependencies, additionalDependencies.devDependencies);
+  Object.assign(packageJson.dependencies, additionalDependencies.dependencies);
+  writeFile('package.json', JSON.stringify(packageJson, null, 2));
+}
 
-writeFile('package.json', JSON.stringify(packageJson, null, 2));
+function setup() {
+  console.log('🔄 Setting up...');
+  addDependencies();
 
-deleteFile('.flowconfig');
-deleteFile('App.js');
-deleteFile('LICENSE');
-deleteFile('README.md');
-deleteFile('setup.js');
+  deleteFile('.flowconfig');
+  deleteFile('App.js');
+  deleteFile('setup.js');
+  deleteFile('dependencies.json');
 
-console.log(`✅ Setup completed!`);
+  console.log(`✅ Setup completed!`);
+}
+
+setup();
